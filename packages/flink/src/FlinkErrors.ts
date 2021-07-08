04abb865ce8789523ad5e583e633843ce8340c07
johanfrost <johan@frostdigital.se>
2021-07-08 10:50:10 +0200

    Adding code to errors, and adding conflict error

--- packages/flink/src/FlinkErrors.ts
+++ packages/flink/src/FlinkErrors.ts
@@ -4,29 +4,45 @@ import { FlinkResponse } from "./FlinkResponse";
 // A marker for FlinkError, but all it means is that data is undefined
 export type FlinkError = undefined;
 
-export function notFound(detail?: string): FlinkResponse<FlinkError> {
+export function notFound(detail?: string, code?: string ): FlinkResponse<FlinkError> {
   return {
     status: 404,
     error: {
       id: v4(),
       title: "Not Found",
       detail: detail || "The requested resource does not exist",
+      code : code || "notFound"
     },
   };
 }
 
-export function badRequest(detail?: string): FlinkResponse<FlinkError> {
+export function conflict(detail?: string, code?: string ): FlinkResponse<FlinkError> {
+  return {
+    status: 409,
+    error: {
+      id: v4(),
+      title: "Conflict",
+      detail: detail || "An identical entity exits",
+      code : code || "conflict"
+    },
+  };
+}
+
+
+
+export function badRequest(detail?: string, code?: string): FlinkResponse<FlinkError> {
   return {
     status: 400,
     error: {
       id: v4(),
       title: "Bad Request",
       detail: detail || "Invalid request",
+      code : code || "badRequest"
     },
   };
 }
 
-export function unauthorized(detail?: string): FlinkResponse<FlinkError> {
+export function unauthorized(detail?: string, code?: string): FlinkResponse<FlinkError> {
   return {
     status: 401,
     error: {
@@ -34,12 +50,14 @@ export function unauthorized(detail?: string): FlinkResponse<FlinkError> {
       title: "Unauthorized",
       detail:
         detail || "User not logged in or or not allowed to access resource",
+      code : code || "badRequest"
     },
   };
 }
 
 export function internalServerError(
-  detail?: string
+  detail?: string,
+  code?: string
 ): FlinkResponse<FlinkError> {
   return {
     status: 500,
@@ -47,6 +65,7 @@ export function internalServerError(
       id: v4(),
       title: "Internal Server Error",
       detail: detail || "Something unexpected went wrong",
+      code : code || "internalServerError"
     },
   };
 }
