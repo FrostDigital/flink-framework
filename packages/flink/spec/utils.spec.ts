ef3635f72b0f7f75825f3c05dcf40d3214823ec8
Joel Soderstrom <joel.soderstrom@gmail.com>
2023-02-13 17:25:39 +0100

    fix: deref anyOf schema

--- packages/flink/spec/utils.spec.ts
+++ packages/flink/spec/utils.spec.ts
@@ -2,91 +2,91 @@ import { JSONSchema7 } from "json-schema";
 import { deRefSchema, getJsDocComment } from "../src/utils";
 
 describe("Utils", () => {
-  describe("deref", () => {
-    it("should de-ref json schema", () => {
-      const dereffedSchema = deRefSchema(
-        jsonSchemas.definitions!.GetCar2_10_ResSchema,
-        jsonSchemas
-      );
+    describe("deref", () => {
+        it("should de-ref json schema", () => {
+            const dereffedSchema = deRefSchema(jsonSchemas.definitions!.GetCar2_10_ResSchema, jsonSchemas);
 
-      const schema = dereffedSchema as JSONSchema7;
+            const schema = dereffedSchema as JSONSchema7;
 
-      // @ts-ignore
-      expect(schema.properties.engine.type).toBe("object");
+            // @ts-ignore
+            expect(schema.properties.engine.type).toBe("object");
 
-      // @ts-ignore
-      expect(schema.properties.model.properties.engine.type).toBe("object");
+            // @ts-ignore
+            expect(schema.properties.model.properties.engine.type).toBe("object");
 
-      // @ts-ignore
-      expect(schema.properties.tires.items.type).toBe("object");
+            // @ts-ignore
+            expect(schema.properties.tires.items.type).toBe("object");
+        });
+
+        it("should de-ref json schema (nested prop)", () => {
+            const dereffedSchema = deRefSchema(jsonSchemas.definitions!.Login, jsonSchemas);
+
+            const schema = dereffedSchema as JSONSchema7;
+
+            // @ts-ignore
+            expect(schema.properties.user.properties.profile.type).toBe("object");
+        });
+
+        it("should de-ref json schema which is an array", () => {
+            const dereffedSchema = deRefSchema(jsonSchemas.definitions!.Cars, jsonSchemas);
+
+            const schema = dereffedSchema as JSONSchema7;
+
+            expect(schema.type).toBe("array");
+
+            // @ts-ignore
+            expect(schema.items.type).toBe("object");
+        });
+
+        it("should de-ref json schema which is an array 2", () => {
+            const dereffedSchema = deRefSchema(jsonSchemas.definitions!.Cars2, jsonSchemas);
+
+            const schema = dereffedSchema as JSONSchema7;
+
+            expect(schema.type).toBe("array");
+
+            // @ts-ignore
+            expect(schema.items.type).toBe("object");
+
+            // @ts-ignore
+            expect(schema.items.properties.model.type).toBe("object");
+        });
+
+        it("should de-ref complex json schema", () => {
+            const dereffedSchema = deRefSchema(jsonSchemasSet2.definitions!.GetStorages_9_ResSchema, jsonSchemasSet2);
+
+            const schema = dereffedSchema as JSONSchema7;
+
+            expect(schema.type).toBe("object");
+
+            // @ts-ignore
+            expect(schema.properties.storages.type).toBe("array");
+
+            // @ts-ignore
+            expect(schema.properties.storages.items.properties.metadata.type).toBe("object");
+        });
+
+        it("should de-ref with anyOf in array", () => {
+            const dereffedSchema = deRefSchema(jsonSchemaSet3.definitions!.GetStorages_9_ResSchema, jsonSchemaSet3);
+
+            const schema = dereffedSchema as JSONSchema7;
+
+            expect(schema.type).toBe("object");
+
+            // @ts-ignore
+            expect(schema.properties.storages.type).toBe("array");
+
+            // @ts-ignore
+            expect(schema.properties.storages.items.anyOf[0].type).toBe("object");
+
+            // @ts-ignore
+            expect(schema.properties.storages.items.anyOf[1].type).toBe("object");
+        });
     });
 
-    it("should de-ref json schema (nested prop)", () => {
-      const dereffedSchema = deRefSchema(
-        jsonSchemas.definitions!.Login,
-        jsonSchemas
-      );
-
-      const schema = dereffedSchema as JSONSchema7;
-
-      // @ts-ignore
-      expect(schema.properties.user.properties.profile.type).toBe("object");
-    });
-
-    it("should de-ref json schema which is an array", () => {
-      const dereffedSchema = deRefSchema(
-        jsonSchemas.definitions!.Cars,
-        jsonSchemas
-      );
-
-      const schema = dereffedSchema as JSONSchema7;
-
-      expect(schema.type).toBe("array");
-
-      // @ts-ignore
-      expect(schema.items.type).toBe("object");
-    });
-
-    it("should de-ref json schema which is an array 2", () => {
-      const dereffedSchema = deRefSchema(
-        jsonSchemas.definitions!.Cars2,
-        jsonSchemas
-      );
-
-      const schema = dereffedSchema as JSONSchema7;
-
-      expect(schema.type).toBe("array");
-
-      // @ts-ignore
-      expect(schema.items.type).toBe("object");
-
-      // @ts-ignore
-      expect(schema.items.properties.model.type).toBe("object");
-    });
-
-    it("should de-ref complex json schema", () => {
-      const dereffedSchema = deRefSchema(
-        jsonSchemasSet2.definitions!.GetStorages_9_ResSchema,
-        jsonSchemasSet2
-      );
-
-      const schema = dereffedSchema as JSONSchema7;
-
-      expect(schema.type).toBe("object");
-
-      // @ts-ignore
-      expect(schema.properties.storages.type).toBe("array");
-
-      // @ts-ignore
-      expect(schema.properties.storages.items.properties.metadata.type).toBe(
-        "object"
-      );
-    });
-  });
-
-  describe("getJsDocComment", () => {
-    it("should strip comment chars and return string", () => {
-      const comment = `
+    describe("getJsDocComment", () => {
+        it("should strip comment chars and return string", () => {
+            const comment = `
 /**
  * Hello world
  * This is another line
@@ -94,172 +94,216 @@ describe("Utils", () => {
  */
       `;
 
-      expect(getJsDocComment(comment)).toBe(
-        "Hello world\nThis is another line\nThis line contains a * (asterisk)"
-      );
+            expect(getJsDocComment(comment)).toBe("Hello world\nThis is another line\nThis line contains a * (asterisk)");
+        });
     });
-  });
 });
 
 const jsonSchemas: JSONSchema7 = {
-  $schema: "http://json-schema.org/draft-07/schema#",
-  $ref: "#/definitions/Schemas",
-  definitions: {
-    GetCar_9_ResSchema: {
-      type: "object",
-      additionalProperties: false,
-      properties: {
-        model: {
-          type: "string",
+    $schema: "http://json-schema.org/draft-07/schema#",
+    $ref: "#/definitions/Schemas",
+    definitions: {
+        GetCar_9_ResSchema: {
+            type: "object",
+            additionalProperties: false,
+            properties: {
+                model: {
+                    type: "string",
+                },
+            },
+            required: ["model"],
         },
-      },
-      required: ["model"],
-    },
-    GetCar2_10_ResSchema: {
-      type: "object",
-      properties: {
-        model: {
-          $ref: "#/definitions/CarModel",
+        GetCar2_10_ResSchema: {
+            type: "object",
+            properties: {
+                model: {
+                    $ref: "#/definitions/CarModel",
+                },
+                engine: {
+                    $ref: "#/definitions/CarEngine",
+                },
+                tires: {
+                    type: "array",
+                    items: {
+                        $ref: "#/definitions/Tire",
+                    },
+                },
+            },
+            required: ["model", "engine"],
+            additionalProperties: false,
         },
-        engine: {
-          $ref: "#/definitions/CarEngine",
+        CarModel: {
+            type: "object",
+            properties: {
+                name: {
+                    type: "string",
+                },
+                engine: {
+                    $ref: "#/definitions/CarEngine",
+                },
+            },
+            required: ["name"],
+            additionalProperties: false,
         },
-        tires: {
-          type: "array",
-          items: {
-            $ref: "#/definitions/Tire",
-          },
+        CarEngine: {
+            type: "object",
+            properties: {
+                name: {
+                    type: "string",
+                },
+            },
+            required: ["name"],
+            additionalProperties: false,
         },
-      },
-      required: ["model", "engine"],
-      additionalProperties: false,
-    },
-    CarModel: {
-      type: "object",
-      properties: {
-        name: {
-          type: "string",
+        Tire: {
+            type: "object",
+            properties: {
+                name: {
+                    type: "string",
+                },
+            },
+            required: ["name"],
+            additionalProperties: false,
         },
-        engine: {
-          $ref: "#/definitions/CarEngine",
+        Cars: {
+            type: "array",
+            items: {
+                $ref: "#/definitions/CarModel",
+            },
         },
-      },
-      required: ["name"],
-      additionalProperties: false,
-    },
-    CarEngine: {
-      type: "object",
-      properties: {
-        name: {
-          type: "string",
+        Cars2: {
+            type: "array",
+            items: {
+                type: "object",
+                properties: {
+                    model: {
+                        $ref: "#/definitions/CarModel",
+                    },
+                },
+            },
         },
-      },
-      required: ["name"],
-      additionalProperties: false,
-    },
-    Tire: {
-      type: "object",
-      properties: {
-        name: {
-          type: "string",
-        },
-      },
-      required: ["name"],
-      additionalProperties: false,
-    },
-    Cars: {
-      type: "array",
-      items: {
-        $ref: "#/definitions/CarModel",
-      },
-    },
-    Cars2: {
-      type: "array",
-      items: {
-        type: "object",
-        properties: {
-          model: {
-            $ref: "#/definitions/CarModel",
-          },
-        },
-      },
-    },
 
-    Login: {
-      type: "object",
-      additionalProperties: false,
-      properties: {
-        status: {
-          type: "string",
-          enum: ["success", "failed", "requiresValidation"],
+        Login: {
+            type: "object",
+            additionalProperties: false,
+            properties: {
+                status: {
+                    type: "string",
+                    enum: ["success", "failed", "requiresValidation"],
+                },
+                user: {
+                    type: "object",
+                    properties: {
+                        _id: {
+                            type: "string",
+                        },
+                        username: {
+                            type: "string",
+                        },
+                        token: {
+                            type: "string",
+                        },
+                        profile: {
+                            $ref: "#/definitions/UserProfile",
+                        },
+                    },
+                    required: ["_id", "username", "token", "profile"],
+                    additionalProperties: false,
+                },
+                validationToken: {
+                    type: "string",
+                },
+            },
+            required: ["status"],
         },
-        user: {
-          type: "object",
-          properties: {
-            _id: {
-              type: "string",
-            },
-            username: {
-              type: "string",
-            },
-            token: {
-              type: "string",
-            },
-            profile: {
-              $ref: "#/definitions/UserProfile",
-            },
-          },
-          required: ["_id", "username", "token", "profile"],
-          additionalProperties: false,
-        },
-        validationToken: {
-          type: "string",
-        },
-      },
-      required: ["status"],
-    },
 
-    UserProfile: {
-      type: "object",
+        UserProfile: {
+            type: "object",
+        },
     },
-  },
 };
 
 const jsonSchemasSet2: JSONSchema7 = {
-  // $schema: "http://json-schema.org/draft-07/schema#",
-  // $ref: "#/definitions/Schemas",
-  definitions: {
-    Metadata: {
-      type: "object",
-      properties: {
-        created: { type: "string", format: "date-time" },
-        updated: { type: "string", format: "date-time" },
-      },
-      required: ["created"],
-      additionalProperties: false,
-    },
-
-    GetStorages_9_ResSchema: {
-      type: "object",
-      additionalProperties: false,
-      properties: {
-        storages: {
-          type: "array",
-          items: {
+    // $schema: "http://json-schema.org/draft-07/schema#",
+    // $ref: "#/definitions/Schemas",
+    definitions: {
+        Metadata: {
             type: "object",
             properties: {
-              _id: { type: "string" },
-              name: { type: "string" },
-              metadata: { $ref: "#/definitions/Metadata" },
+                created: { type: "string", format: "date-time" },
+                updated: { type: "string", format: "date-time" },
             },
-            required: ["_id", "name", "metadata"],
+            required: ["created"],
             additionalProperties: false,
-          },
         },
-      },
-      required: ["storages"],
-    },
 
-    UserRole: { type: "string", enum: ["user", "admin", "teamLeader"] },
-  },
+        GetStorages_9_ResSchema: {
+            type: "object",
+            additionalProperties: false,
+            properties: {
+                storages: {
+                    type: "array",
+                    items: {
+                        type: "object",
+                        properties: {
+                            _id: { type: "string" },
+                            name: { type: "string" },
+                            metadata: { $ref: "#/definitions/Metadata" },
+                        },
+                        required: ["_id", "name", "metadata"],
+                        additionalProperties: false,
+                    },
+                },
+            },
+            required: ["storages"],
+        },
+
+        UserRole: { type: "string", enum: ["user", "admin", "teamLeader"] },
+    },
+};
+
+const jsonSchemaSet3: JSONSchema7 = {
+    definitions: {
+        Metadata1: {
+            type: "object",
+            properties: {
+                created: { type: "string", format: "date-time" },
+                updated: { type: "string", format: "date-time" },
+            },
+            required: ["created"],
+            additionalProperties: false,
+        },
+
+        Metadata2: {
+            type: "object",
+            properties: {
+                created: { type: "string", format: "date-time" },
+            },
+            required: ["created"],
+            additionalProperties: false,
+        },
+
+        GetStorages_9_ResSchema: {
+            type: "object",
+            additionalProperties: false,
+            properties: {
+                storages: {
+                    type: "array",
+                    items: {
+                        anyOf: [
+                            {
+                                $ref: "#/definitions/Metadata1",
+                            },
+                            {
+                                $ref: "#/definitions/Metadata2",
+                            },
+                        ],
+                    },
+                },
+            },
+            required: ["storages"],
+        },
+
+        UserRole: { type: "string", enum: ["user", "admin", "teamLeader"] },
+    },
 };
--- packages/flink/src/utils.ts
+++ packages/flink/src/utils.ts
@@ -7,60 +7,57 @@ import { log } from "./FlinkLog";
 import { FlinkResponse } from "./FlinkResponse";
 
 export function handlersPath(appRoot: string) {
-  return join(appRoot, "src", "handlers");
+    return join(appRoot, "src", "handlers");
 }
 
 export function schemasPath(appRoot: string) {
-  return join(appRoot, "src", "schemas");
+    return join(appRoot, "src", "schemas");
 }
 
-export function isRouteMatch(
-  req: Request,
-  routes: { method: HttpMethod; path: string }[]
-) {
-  const match = routes.find(({ method, path }) => {
-    const sameMethod = req.method.toLowerCase() === method;
-    const samePath = req.route.path === path;
-    return sameMethod && samePath;
-  });
+export function isRouteMatch(req: Request, routes: { method: HttpMethod; path: string }[]) {
+    const match = routes.find(({ method, path }) => {
+        const sameMethod = req.method.toLowerCase() === method;
+        const samePath = req.route.path === path;
+        return sameMethod && samePath;
+    });
 
-  return !!match;
+    return !!match;
 }
 
 export function isError(message: FlinkResponse) {
-  return message.status && message.status > 399;
+    return message.status && message.status > 399;
 }
 
 export async function getHandlerFiles(appRoot: string) {
-  try {
-    return await tinyGlob(`**/*.ts`, {
-      cwd: handlersPath(appRoot),
-      absolute: true,
-    });
-  } catch (err) {
-    log.debug(`Failed getting handler files: ${err}`);
-    return [];
-  }
+    try {
+        return await tinyGlob(`**/*.ts`, {
+            cwd: handlersPath(appRoot),
+            absolute: true,
+        });
+    } catch (err) {
+        log.debug(`Failed getting handler files: ${err}`);
+        return [];
+    }
 }
 
 export async function getSchemaFiles(appRoot: string) {
-  try {
-    return await tinyGlob(`**/*.ts`, {
-      cwd: schemasPath(appRoot),
-      absolute: true,
-    });
-  } catch (err) {
-    return [];
-  }
+    try {
+        return await tinyGlob(`**/*.ts`, {
+            cwd: schemasPath(appRoot),
+            absolute: true,
+        });
+    } catch (err) {
+        return [];
+    }
 }
 
 export function getCollectionNameForRepo(repoFilename: string) {
-  return repoFilename.replace("Repo.ts", "").toLowerCase();
+    return repoFilename.replace("Repo.ts", "").toLowerCase();
 }
 
 export function getRepoInstanceName(fn: string) {
-  const [name] = fn.split(".ts");
-  return name.charAt(0).toLowerCase() + name.substr(1);
+    const [name] = fn.split(".ts");
+    return name.charAt(0).toLowerCase() + name.substr(1);
 }
 
 /**
@@ -68,17 +65,17 @@ export function getRepoInstanceName(fn: string) {
  * if it starts with i.e "GetFoo"
  */
 export function getHttpMethodFromHandlerName(handlerFilename: string) {
-  if (handlerFilename.includes(sep)) {
-    const split = handlerFilename.split(sep);
-    handlerFilename = split[split.length - 1];
-  }
+    if (handlerFilename.includes(sep)) {
+        const split = handlerFilename.split(sep);
+        handlerFilename = split[split.length - 1];
+    }
 
-  handlerFilename = handlerFilename.toLocaleLowerCase();
+    handlerFilename = handlerFilename.toLocaleLowerCase();
 
-  if (handlerFilename.startsWith(HttpMethod.get)) return HttpMethod.get;
-  if (handlerFilename.startsWith(HttpMethod.post)) return HttpMethod.post;
-  if (handlerFilename.startsWith(HttpMethod.put)) return HttpMethod.put;
-  if (handlerFilename.startsWith(HttpMethod.delete)) return HttpMethod.delete;
+    if (handlerFilename.startsWith(HttpMethod.get)) return HttpMethod.get;
+    if (handlerFilename.startsWith(HttpMethod.post)) return HttpMethod.post;
+    if (handlerFilename.startsWith(HttpMethod.put)) return HttpMethod.put;
+    if (handlerFilename.startsWith(HttpMethod.delete)) return HttpMethod.delete;
 }
 
 /**
@@ -89,75 +86,84 @@ export function getHttpMethodFromHandlerName(handlerFilename: string) {
  * @param jsonSchemas
  * @returns
  */
-export function deRefSchema(
-  schemaToDeRef: JSONSchema7Definition,
-  jsonSchemas: JSONSchema7
-) {
-  if (typeof schemaToDeRef === "boolean") {
+export function deRefSchema(schemaToDeRef: JSONSchema7Definition, jsonSchemas: JSONSchema7) {
+    if (typeof schemaToDeRef === "boolean") {
+        return schemaToDeRef;
+    }
+
+    if (schemaToDeRef.type === "array") {
+        const items = schemaToDeRef.items as JSONSchema7;
+
+        if (items.$ref) {
+            const [_0, _1, defKey] = items.$ref.split("/");
+            const refedSchema = (jsonSchemas.definitions || {})[defKey];
+
+            if (refedSchema) {
+                schemaToDeRef.items = deRefSchema(refedSchema, jsonSchemas);
+            } else {
+                console.warn(`Failed to find deref ${schemaToDeRef.$ref}`);
+            }
+        } else {
+            schemaToDeRef.items = deRefSchema(schemaToDeRef.items as JSONSchema7, jsonSchemas);
+        }
+    } else if (schemaToDeRef.properties) {
+        for (const k in schemaToDeRef.properties) {
+            let prop = schemaToDeRef.properties[k];
+
+            if (typeof prop === "boolean") {
+                continue;
+            }
+
+            if (prop.$ref) {
+                const [_0, _1, defKey] = prop.$ref.split("/");
+                const refedSchema = (jsonSchemas.definitions || {})[defKey];
+                if (refedSchema) {
+                    schemaToDeRef.properties[k] = deRefSchema(refedSchema, jsonSchemas);
+                } else {
+                    console.warn(`Failed to find deref ${prop.$ref}`);
+                }
+            } else if (prop.type === "array" && (prop.items as JSONSchema7).$ref) {
+                const [_0, _1, defKey] = (prop.items as JSONSchema7).$ref!.split("/");
+                const refedSchema = (jsonSchemas.definitions || {})[defKey];
+                if (refedSchema) {
+                    (schemaToDeRef.properties[k] as JSONSchema7).items = deRefSchema(refedSchema, jsonSchemas);
+                } else {
+                    console.warn(`Failed to find deref ${prop.$ref}`);
+                }
+            } else if (prop.type === "object" || prop.type === "array") {
+                schemaToDeRef.properties[k] = deRefSchema(prop, jsonSchemas);
+            }
+        }
+    } else if (schemaToDeRef.anyOf) {
+        let i = 0;
+        for (const anyOf of schemaToDeRef.anyOf) {
+            const anyOfSchema = anyOf as JSONSchema7;
+
+            if (anyOfSchema.$ref) {
+                const [_0, _1, defKey] = anyOfSchema.$ref.split("/");
+                const refedSchema = (jsonSchemas.definitions || {})[defKey];
+                if (refedSchema) {
+                    schemaToDeRef.anyOf[i] = deRefSchema(refedSchema, jsonSchemas);
+                } else {
+                    console.warn(`Failed to find deref ${anyOfSchema.$ref}`);
+                }
+            }
+
+            i++;
+        }
+    }
+
     return schemaToDeRef;
-  }
-
-  if (schemaToDeRef.type === "array") {
-    const items = schemaToDeRef.items as JSONSchema7;
-
-    if (items.$ref) {
-      const [_0, _1, defKey] = items.$ref.split("/");
-      const refedSchema = (jsonSchemas.definitions || {})[defKey];
-
-      if (refedSchema) {
-        schemaToDeRef.items = deRefSchema(refedSchema, jsonSchemas);
-      } else {
-        console.warn(`Failed to find deref ${schemaToDeRef.$ref}`);
-      }
-    } else {
-      schemaToDeRef.items = deRefSchema(
-        schemaToDeRef.items as JSONSchema7,
-        jsonSchemas
-      );
-    }
-  } else if (schemaToDeRef.properties) {
-    for (const k in schemaToDeRef.properties) {
-      let prop = schemaToDeRef.properties[k];
-
-      if (typeof prop === "boolean") {
-        continue;
-      }
-
-      if (prop.$ref) {
-        const [_0, _1, defKey] = prop.$ref.split("/");
-        const refedSchema = (jsonSchemas.definitions || {})[defKey];
-        if (refedSchema) {
-          schemaToDeRef.properties[k] = deRefSchema(refedSchema, jsonSchemas);
-        } else {
-          console.warn(`Failed to find deref ${prop.$ref}`);
-        }
-      } else if (prop.type === "array" && (prop.items as JSONSchema7).$ref) {
-        const [_0, _1, defKey] = (prop.items as JSONSchema7).$ref!.split("/");
-        const refedSchema = (jsonSchemas.definitions || {})[defKey];
-        if (refedSchema) {
-          (schemaToDeRef.properties[k] as JSONSchema7).items = deRefSchema(
-            refedSchema,
-            jsonSchemas
-          );
-        } else {
-          console.warn(`Failed to find deref ${prop.$ref}`);
-        }
-      } else if (prop.type === "object" || prop.type === "array") {
-        schemaToDeRef.properties[k] = deRefSchema(prop, jsonSchemas);
-      }
-    }
-  }
-  return schemaToDeRef;
 }
 
 export function getJsDocComment(comment: string) {
-  const rows = comment.split("\n").map((line) => {
-    line = line.trim();
-    if (line.startsWith("//")) {
-      return line.replace("//", line).trim();
-    }
-    return line.replace(/\/\*\*|\*\/|\*/, "").trim();
-  });
+    const rows = comment.split("\n").map((line) => {
+        line = line.trim();
+        if (line.startsWith("//")) {
+            return line.replace("//", line).trim();
+        }
+        return line.replace(/\/\*\*|\*\/|\*/, "").trim();
+    });
 
-  return rows.join("\n").trim();
+    return rows.join("\n").trim();
 }
