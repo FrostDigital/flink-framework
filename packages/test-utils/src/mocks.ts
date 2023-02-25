302673bb95dbf322870e92637308cd6955ca265b
Joel Soderstrom <joel.soderstrom@gmail.com>
2023-02-25 22:13:52 +0100

    feat: Options and utils to improve testing
    Add FlinkApp option to start app without web server
    Adds util methods to create a mock req.

---
+++ packages/test-utils/src/mocks.ts
@@ -0,0 +1,40 @@
+import { FlinkAuthPlugin, FlinkRequest } from "@flink-app/flink";
+
+interface TestFlinkRequest<T, P, Q> extends Omit<Partial<FlinkRequest<T, P, Q>>, "body" | "params" | "query"> {
+    body?: T;
+    params?: P;
+    query?: Q;
+}
+
+/**
+ * Creates a mocked FlinkRequest object where only essential properties are required.
+ * Will convert req body to JSON to ensure the body is a plain object.
+ * @param req
+ * @returns
+ */
+export function mockReq<T, P, Q>(req?: TestFlinkRequest<T, P, Q>): FlinkRequest<T, P, Q> {
+    const aMockReq = {
+        get: (headerName: string) => req?.headers?.[headerName],
+        body: req?.body,
+    };
+
+    if (!req) {
+        return aMockReq as any;
+    }
+
+    req.body = req.body ? JSON.parse(JSON.stringify(req.body)) : undefined;
+
+    return aMockReq as any;
+}
+
+/**
+ * Auth plugin used for testing handlers.
+ * Will allow all requests.
+ * @returns
+ */
+export function noOpAuthPlugin(): FlinkAuthPlugin {
+    return {
+        authenticateRequest: async () => true,
+        createToken: async () => "mock-token",
+    };
+}
