9a046a1b9fd57660a35c86d578ec717ff871381c
Joel Soderstrom <joel.soderstrom@gmail.com>
2023-05-13 14:41:00 +0200

    fix: exposes so password options
    Adds so password reset in generic auth can
    be a function to make it more configurable

--- packages/generic-auth-plugin/src/handlers/UserPasswordResetStart.ts
+++ packages/generic-auth-plugin/src/handlers/UserPasswordResetStart.ts
@@ -7,67 +7,59 @@ import { UserPasswordResetStartReq } from "../schemas/UserPasswordResetStartReq"
 import { UserPasswordResetStartResPublic } from "../schemas/UserPasswordResetStartResPublic";
 
 const postPasswordResetStartHandler: Handler<
-  FlinkContext<genericAuthContext & emailPluginContext>,
-  UserPasswordResetStartReq,
-  UserPasswordResetStartResPublic
+    FlinkContext<genericAuthContext & emailPluginContext>,
+    UserPasswordResetStartReq,
+    UserPasswordResetStartResPublic
 > = async ({ ctx, req, origin }) => {
-  let pluginName = origin || "genericAuthPlugin";
-  let repo = ctx.repos[(<any>ctx.plugins)[pluginName].repoName];
+    let pluginName = origin || "genericAuthPlugin";
 
-  if (ctx.plugins.emailPlugin?.client == null) {
-    return internalServerError(
-      "Email plugin have to be initialized to use /password/reset"
-    );
-  }
+    const genericAuthPlugin = (<any>ctx.plugins)[pluginName] as genericAuthContext["genericAuthPlugin"];
 
-  if (ctx.plugins.genericAuthPlugin.passwordResetSettings == null) {
-    return internalServerError(
-      "Password reset settings is needed to use /password/reset"
-    );
-  }
+    let repo = ctx.repos[genericAuthPlugin.repoName];
 
-  const { jwtSecret, numberOfDigits, lifeTime } =
-  (<any>ctx.plugins)[pluginName].passwordResetSettings.code;
+    if (ctx.plugins.emailPlugin?.client == null) {
+        return internalServerError("Email plugin have to be initialized to use /password/reset");
+    }
 
-   
+    if (!genericAuthPlugin.passwordResetSettings) {
+        return internalServerError("Password reset settings is needed to use /password/reset");
+    }
 
-  const resp = await ctx.plugins.genericAuthPlugin.passwordResetStart(
-    repo,
-    <JwtAuthPlugin>ctx.auth,
-    jwtSecret,
-    req.body.username,
-    numberOfDigits,
-    lifeTime
-  );
+    const { jwtSecret, numberOfDigits, lifeTime } = genericAuthPlugin.passwordResetSettings.code;
 
-  if (resp.status != "success") {
-    return { data: { status: resp.status } };
-  }
+    const resp = await genericAuthPlugin.passwordResetStart(repo, <JwtAuthPlugin>ctx.auth, jwtSecret, req.body.username, numberOfDigits, lifeTime);
 
-  const emailSettings =
-  (<any>ctx.plugins)[pluginName].passwordResetSettings.email;
+    if (resp.status != "success") {
+        return { data: { status: resp.status } };
+    }
 
-  const emailCtx = {
-    code: resp.code,
-    passwordResetToken: resp.passwordResetToken,
-    username: req.body.username,
-    profile: resp.profile,
-  };
+    const emailCtx = {
+        code: resp.code,
+        passwordResetToken: resp.passwordResetToken,
+        username: req.body.username,
+        profile: resp.profile,
+    };
 
-  const subject = Handlebars.compile(emailSettings.subject)(emailCtx);
-  const html = Handlebars.compile(emailSettings.html)(emailCtx);
-  const email = req.body.username;
+    let emailSettings = genericAuthPlugin.passwordResetSettings.email;
 
-  await ctx.plugins.emailPlugin.client.send({
-    from: emailSettings.from_address,
-    to: [email],
-    subject,
-    html,
-  });
+    if (typeof emailSettings == "function") {
+        emailSettings = emailSettings(emailCtx);
+    }
 
-  return {
-    data: { status: "success", passwordResetToken: resp.passwordResetToken },
-  };
+    const subject = Handlebars.compile(emailSettings.subject)(emailCtx);
+    const html = Handlebars.compile(emailSettings.html)(emailCtx);
+    const email = req.body.username;
+
+    await ctx.plugins.emailPlugin.client.send({
+        from: emailSettings.from_address,
+        to: [email],
+        subject,
+        html,
+    });
+
+    return {
+        data: { status: "success", passwordResetToken: resp.passwordResetToken },
+    };
 };
 
 export default postPasswordResetStartHandler;
--- packages/generic-auth-plugin/src/schemas/UserPasswordResetSettings.ts
+++ packages/generic-auth-plugin/src/schemas/UserPasswordResetSettings.ts
@@ -1,14 +1,25 @@
-export interface UserPasswordResetSettings {
-  email: {
+import { UserProfile } from "./UserProfile";
+
+interface EmailConfig {
     from_address: string;
     subject: string;
     html: string;
-  };
-  code: {
-    numberOfDigits: number;
-    lifeTime: string;
-    jwtSecret: string;
-  };
-  enablePasswordResetForm?: boolean;
-  passwordResetForm?: string;
+}
+
+interface EmailCtx {
+    code?: string;
+    passwordResetToken?: string;
+    username: string;
+    profile?: UserProfile;
+}
+
+export interface UserPasswordResetSettings {
+    email: EmailConfig | ((emailCtx: EmailCtx) => EmailConfig);
+    code: {
+        numberOfDigits: number;
+        lifeTime: string;
+        jwtSecret: string;
+    };
+    enablePasswordResetForm?: boolean;
+    passwordResetForm?: string;
 }
